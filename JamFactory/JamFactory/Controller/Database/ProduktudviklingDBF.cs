220beeef8c80bc3deed0ec53b1c2aec071872d61
fred3760 <fred3760@edu.eal.dk>
2015-03-03 12:34:14 +0100

    getallingredients

---
+++ JamFactory/JamFactory/Controller/Database/ProduktudviklingDBF.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace JamFactory.Controller.Database
+{
+    class ProduktudviklingDBF
+    {
+        string DBConnectionString = "Server=ealdb1.eal.local;" + "Database=EJL20_DB;" + "User Id=ejl20_usr;" + "Password=Baz1nga20;";
+
+        public List<Model.Ingredient> GetAllIngredients()
+        {
+            List<Model.Ingredient> ingredients = new List<Model.Ingredient>();
+
+            SqlConnection SqlConnection = new SqlConnection(DBConnectionString);
+
+            try
+            {
+                SqlConnection.Open();
+
+                SqlCommand cmd = new SqlCommand("0400_get_ingredient", SqlConnection);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read() && rdr.HasRows)
+                {
+                    ingredients.Add(new Model.Ingredient(rdr["Name"].ToString(), int.Parse(rdr["ID"].ToString())));
+                }
+            }
+            catch (SqlException e)
+            {
+                System.Windows.MessageBox.Show(e.Message);
+
+            }
+            finally
+            {
+                SqlConnection.Close();
+                SqlConnection.Dispose();
+            }
+
+            return ingredients;
+        }
+    }
+}
---
+++ JamFactory/JamFactory/Model/Ingredient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JamFactory.Model
+{
+    class Ingredient
+    {
+        public string Name { get; set; }
+        public int ID { get; set; }
+        public Ingredient(string name, int id)
+        {
+            Name = name;
+            ID = id; 
+        }
+    }
+}
